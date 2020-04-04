2d82b7e483a1c5795a293a2017e4015d4cbf9aa3
JGoliszewski <45105582+jgoliszewski@users.noreply.github.com>
2020-04-04 18:47:20 +0200

    appearance upgrade

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -1,34 +1,33 @@
 import time
-
+green = '#00e30b'
 def bubbleSort(data, drawData, timeTick):
 	for _ in range(len(data)-1):
 		for i in range(len(data)-1):
+			drawData(data, [green if x == i or x == i+1 else 'red' for x in range(len(data))])
 			if data[i] > data[i+1]:
-				drawData(data, ['green' if x == i or x == i+1 else 'red' for x in range(len(data))])
 				data[i], data[i+1] = data[i+1], data[i]
 				time.sleep(timeTick/2)
-				drawData(data, ['green' if x == i or x == i+1 else 'red' for x in range(len(data))])
+				drawData(data, [green if x == i or x == i+1 else 'red' for x in range(len(data))])
 				time.sleep(timeTick/2)
 			else:
-				drawData(data, ['green' if x == i or x == i+1 else 'red' for x in range(len(data))])
 				time.sleep(timeTick)
-	drawData(data, ['green' for x in range(len(data))])
+	drawData(data, ['#00e30b' for x in range(len(data))], finished=True)
 
 def bubbleSortPlus(data, drawData, timeTick):
 	swap = True
+	sort = []
 	for j in range(len(data)-1):
 		if swap:
 			swap = False
 			for i in range(len(data)-j-1):
+				drawData(data, [green if x == i or x == i+1 or x in sort else 'red' for x in range(len(data))])
 				if data[i] > data[i+1]:
 					swap = True
-					drawData(data, ['green' if x == i or x == i+1 else 'red' for x in range(len(data))])
 					data[i], data[i+1] = data[i+1], data[i]
 					time.sleep(timeTick/2)
-					drawData(data, ['green' if x == i or x == i+1 else 'red' for x in range(len(data))])
+					drawData(data, [green if x == i or x == i+1 or x in sort else 'red' for x in range(len(data))])
 					time.sleep(timeTick/2)
 				else:
-					drawData(data, ['green' if x == i or x == i+1 else 'red' for x in range(len(data))])
 					time.sleep(timeTick)
-
-	drawData(data, ['green' for x in range(len(data))])
+		sort.append(len(data)-j-1)
+	drawData(data, [green for x in range(len(data))], finished=True)
--- sortingVisualizer.py
+++ sortingVisualizer.py
@@ -3,22 +3,24 @@ from tkinter import ttk
 import random
 from sortingAlgorithms import bubbleSort, bubbleSortPlus
 
-
 root = Tk()
 root.title('Sorting Algorithm Visualiser')
 root.maxsize(900,600)
-root.config(bg='black')
+root.config(bg='#b3b3b3')
+
 
 #variables
 algorithms = ['Bubble Sort','Bubble Sort Plus']
 selected_alg = StringVar()
 data = []
+font = ('Arial',15)
+bgColor = 'white'
 
 #functions
-def drawData(data, colorArray):
+def drawData(data, colorArray, finished=False):
 	canvas.delete('all')
 	canvas_height = 380
-	canvas_width = 600
+	canvas_width = 800
 	x_width = (canvas_width) / (len(data) + 1)
 	spacing = 5
 	offset = x_width//2
@@ -33,7 +35,8 @@ def drawData(data, colorArray):
  
 		canvas.create_rectangle(x0, y0, x1, y1, fill=colorArray[i])
 		canvas.create_text(x0+x_width//3, y0, anchor=SW, text=str(data[i]))
-
+	if finished:
+		canvas.create_text(canvas_width//2, canvas_height//3, text='Finished!',font=('Arial',50))
 	root.update_idletasks()
 
 def Generate():
@@ -54,38 +57,84 @@ def startAlgorithm():
 		bubbleSort(data, drawData, speedScale.get())
 	if selected_alg.get() == "Bubble Sort Plus":
 		bubbleSortPlus(data, drawData, speedScale.get())
-	print(selected_alg.get())
 
 #frame loyout
-UI_frame = Frame(root, width=600, height=200, bg='grey')
+UI_frame = Frame(root, width=800, height=200, bg='#a7a7a7')
 UI_frame.grid(row=0, column=0, padx=10, pady=5)
 
-canvas = Canvas(root, width=600, height=380, bg='white')
+canvas = Canvas(root, width=800, height=380, bg='#f2f2f2')
 canvas.grid(row=1, column=0, padx=10, pady=5)
 
 #User Interface
 #Row[0]
-Label(UI_frame, text='Algorithm: ', bg='gray').grid(row=0, column=0, sticky=W)
-algMenu = ttk.Combobox(UI_frame, textvariable=selected_alg, values=[alg for alg in algorithms])
-algMenu.grid(row=0, column=1, padx=5, pady=5)
-algMenu.current(0)
 
-speedScale = Scale(UI_frame, from_=0.1, to=2.0,length=200, digits=2, resolution=0.1, orient=HORIZONTAL, label='Select speed')
+algMenu = ttk.Combobox(UI_frame, 
+	font = font,
+	textvariable=selected_alg, 
+	values=[alg for alg in algorithms])
+algMenu.grid(row=0, columnspan=2, padx=5, pady=5)
+algMenu.current(1)
+
+speedScale = Scale(UI_frame,
+				from_=0.1,
+				to=2.0,
+				length=150,
+				digits=2, 
+				resolution=0.1,
+				orient=HORIZONTAL, 
+				label='Select speed',
+				font=font,
+				relief='groove',
+				bg=bgColor,
+				highlightbackground='black')
 speedScale.grid(row=0, column=2, padx=5, pady=5)
 
-Button(UI_frame, text="Start", command=startAlgorithm, bg='red').grid(row=0, column=3, padx=5, pady=5)
+
+Button(UI_frame, 
+	text="    Start    ", 
+	font=font,
+	justify='center',
+	command=startAlgorithm, 
+	bg='#00e30b',
+	).grid(row=0, column=3, padx=5, pady=5)
 
 #Row[1]
 
-sizeEntry = Scale(UI_frame, from_=5, to=25, orient=HORIZONTAL, label='Data size')
+sizeEntry = Scale(UI_frame, 
+	from_=5, 
+	to=25, 
+	orient=HORIZONTAL, 
+	label='Data size', 
+	font=font,
+	bg=bgColor,
+	highlightbackground='black')
 sizeEntry.grid(row=1, column=0, padx=5, pady=5)
 
-minEntry = Scale(UI_frame, from_=0, to=10, orient=HORIZONTAL, label='Min Value')
-minEntry.grid(row=1, column=1, padx=5, pady=5)
+minEntry = Scale(UI_frame, 
+	from_=0, 
+	to=10, 
+	orient=HORIZONTAL, 
+	label='Min Value',
+	font=font,
+	bg=bgColor,
+	highlightbackground='black')
+minEntry.grid(row=1, column=1, padx=5, pady=5, sticky=E)
 
-maxEntry = Scale(UI_frame, from_=10, to=100, orient=HORIZONTAL, label='Max Value')
+maxEntry = Scale(UI_frame, 
+	from_=10, 
+	to=100, 
+	orient=HORIZONTAL, 
+	label='Max Value',
+	font=font,
+	bg=bgColor,
+	highlightbackground='black')
 maxEntry.grid(row=1, column=2, padx=5, pady=5)
 
-Button(UI_frame, text="Generate", command=Generate, bg='white').grid(row=1, column=3, padx=5, pady=5)
+Button(UI_frame, 
+	text="Generate", 
+	command=Generate, 
+	bg='#15b4ea', 
+	font=font
+	).grid(row=1, column=3, padx=5, pady=5)
 
 root.mainloop()
